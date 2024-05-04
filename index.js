15bcc576dcf996076a6afb7e12c67dd9a3696780
BryanGIG <contacts.bryangig@gmail.com>
2024-05-04 10:59:16 +0700

    Fetch from fandom page

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const { gotScraping } = require('got-scraping');
-const fs =  require('fs');
-const buffer =  require('buffer');
+const https = require('https');
+const fs = require('fs');
+const cheerio = require('cheerio');
+
+var download = function(url, dest, cb) {
+    var file = fs.createWriteStream(dest);
+    var request = https.get(url, function(response) {
+      response.pipe(file);
+      file.on('finish', function() {
+        file.close(cb);  // close() is async, call cb after close completes.
+      });
+    }).on('error', function(err) { // Handle errors
+      fs.unlink(dest); // Delete the file async. (But we don't check the result)
+      if (cb) cb(err.message);
+    });
+  };
 
 async function start() {
     const response = await gotScraping({
-        responseType: 'json',
-        url: 'https://mapi.mobilelegends.com/hero/list',
+        url: 'https://mobile-legends.fandom.com/wiki/List_of_heroes',
     });
 
-    const data = response.body["data"];
-    for (const key in data) {
-        if (Object.hasOwnProperty.call(data, key)) {
-            const element = data[key];
-            const image = await gotScraping({
-                responseType: 'buffer',
-                url: `https:${element["key"]}`
-            });
-            var filename = image.url.split('/').pop().split('#')[0].split('?')[0];
-
-            fs.writeFile(`HeroIcon/${element["name"]}.${filename.split(".")[1]}`, image.body, "binary", (err) => {
-                if (err) throw err;
-                console.log(`${element["name"]}: ${filename}`);
-            });
-            
-
-        }
-    }
+    const $ = cheerio.load(response.body);
+    const listHero = ($('.fandom-table > tbody td:nth-child(1) > a'));
+    listHero.each(function (i, e) {
+        const image = $(e).find("img");
+        const nameHero = $(e).attr("title");
+        const fileExtension = image.attr("data-image-name").split(".").pop();
+        
+        download(image.attr("data-src"),`HeroIcon/${nameHero}.${fileExtension}`, (err) => {
+            if (err) throw err;
+            console.log(`${nameHero}.${fileExtension}`);
+        });
+    });
 }
 
 start();
